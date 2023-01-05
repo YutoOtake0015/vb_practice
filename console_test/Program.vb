ebcc1aacfaa4e74e10b0c300f2247e5925f31578
YutoOtake <yuto.programming@gmail.com>
2023-01-06 06:11:39 +0900

    Animalクラスヲインスタンス化

--- console_test/Program.vb
+++ console_test/Program.vb
@@ -2,7 +2,15 @@ Imports System
 
 Module Program
     Sub Main(args As String())
-        Console.WriteLine("Hello World!")
+        Dim animal1 As New Animal()
+        Dim animal2 As New Animal()
+
+        animal1.age = 20
+        animal2.age = 33
+
+        Console.WriteLine($"{NameOf(animal1)}‚Ì”N—î‚Í{animal1.age}‚Å‚·")
+        Console.WriteLine($"{NameOf(animal2)}‚Ì”N—î‚Í{animal2.age}‚Å‚·")
+
 
         Console.ReadLine()
     End Sub
